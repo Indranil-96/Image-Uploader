46484e9172b10e44d2262312ede21020141560ef
Indranil Hazra <hazraindranil399@gmail.com>
2024-09-30 12:06:57 +0530

    Express Session Setup

--- app.js
+++ app.js
@@ -9,3 +9,30 @@ const express=require("express");
 
 const application=express();
 const Port= process.env.Port || 5000;
+
+//Middleware
+application.use(express.urlencoded());
+application.use(express.json());
+
+application.use(
+    session({
+        secret: "My secret key",
+        saveUninitialized: true,
+        resave: false,
+}));
+
+application.use((req,res,next)=>{
+    res.locals.message = req.session.message;
+    delete req.session.message;
+    next();
+})
+
+
+
+
+
+// Routing
+
+application.get('/',(req,res)=>{
+
+});
